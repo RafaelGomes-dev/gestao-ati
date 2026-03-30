9d898de30d9edb8854a87c1418182a648d973e51
RafaelGomes-dev <rafinhagomes1231@gmail.com>
2026-03-30 15:33:29 -0300

    fix: configura HTTPS e servidores no Swagger

--- src/main/java/com/Rafael/gestao_ati/config/SwaggerConfig.java
+++ src/main/java/com/Rafael/gestao_ati/config/SwaggerConfig.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,9 +19,13 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Gestão de ativos de TI")
+                        .title("Gestão de Ativos de TI")
                         .version("1.0")
                         .description("API para gerenciamento de ativos, perifericos e licenças de software"))
+                .servers(List.of(
+                        new Server().url("https://gestao-ati-production.up.railway.app").description("Produção"),
+                        new Server().url("http://localhost:8080").description("Local")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication",
@@ -26,9 +33,5 @@ public class SwaggerConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
-
-
-
     }
-
 }
